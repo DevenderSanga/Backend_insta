58d75851a85990c961b96200740907e54e609c30
DevenderSanga <devendersanga143@gmail.com>
2023-06-02 10:12:36 +0530

    added all files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose")
+const router=require('./router/instaclone')
+let BASE_URL= process.env.BASE_URL;
+let DB= process.env.DB;
+const PORT = process.env.PORT
+const app = express();
+const cors = require("cors")
+app.use(cors());
+app.use(express.json());
+app.use(router)
+
+mongoose.connect(BASE_URL+DB)
+.then(res=>console.log("connection is successfull"))
+.catch(err=>console.log("connection not sucess",err))
+
+
+app.listen(PORT,()=>{
+    console.log(`listening the port ${PORT}`)
+})
